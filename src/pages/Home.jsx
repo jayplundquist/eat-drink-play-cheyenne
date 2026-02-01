a1e12b30f7d5b6e2065b92ed842ea7e3e6eb2737
Jay Lundquist <jay.p.lundquist@gmail.com>
2026-02-01 16:53:29 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -502,18 +502,26 @@ export default function Home() {
                 )}
                 </section>
 
-                {/* Suggestions Button */}
-      <section className="max-w-6xl mx-auto px-4 sm:px-6 py-4 text-center">
-        <Dialog open={suggestionOpen} onOpenChange={setSuggestionOpen}>
-          <DialogTrigger asChild>
-            <Button 
-              variant="outline" 
-              className="border-2 border-amber-800 text-amber-800 hover:bg-amber-50"
-            >
-              <Lightbulb className="w-5 h-5 mr-2" />
-              Have a Suggestion?
-            </Button>
-          </DialogTrigger>
+                {/* Export & Suggestions */}
+                <section className="max-w-6xl mx-auto px-4 sm:px-6 py-4 flex gap-3 justify-center">
+                <Button 
+                onClick={exportVenuesToExcel}
+                variant="outline" 
+                className="border-2 border-amber-800 text-amber-800 hover:bg-amber-50"
+                >
+                <Download className="w-5 h-5 mr-2" />
+                Export Venues to Excel
+                </Button>
+                <Dialog open={suggestionOpen} onOpenChange={setSuggestionOpen}>
+                <DialogTrigger asChild>
+                <Button 
+                variant="outline" 
+                className="border-2 border-amber-800 text-amber-800 hover:bg-amber-50"
+                >
+                <Lightbulb className="w-5 h-5 mr-2" />
+                Have a Suggestion?
+                </Button>
+                </DialogTrigger>
           <DialogContent>
             <DialogHeader>
               <DialogTitle>Share Your Suggestion</DialogTitle>

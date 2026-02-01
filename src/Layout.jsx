feb41c3f34b96eaff9d3aa49a8bebc6933327e25
Jay Lundquist <jay.p.lundquist@gmail.com>
2026-02-01 05:55:23 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { base44 } from '@/api/base44Client';
+import NotificationManager from '@/components/NotificationManager';
 import { Button } from "@/components/ui/button";
 import { Toaster } from "sonner";
 import { 
